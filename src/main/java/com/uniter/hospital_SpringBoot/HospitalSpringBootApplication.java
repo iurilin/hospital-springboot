70aae2d082fa6bc7a09468266900485296d5c5fb
Iuri Lim <audazzk@gmail.com>
2025-06-19 14:21:41 -0300

    Projeto Criado

---
+++ src/main/java/com/uniter/hospital_SpringBoot/HospitalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.uniter.hospital_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniter/hospital_SpringBoot/HospitalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniter.hospital_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
